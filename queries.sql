9aebe13dd2009f57a4edea6cc0153e99d1536512
allybreeden <allybreeden@gmail.com>
2023-06-07 19:18:02 -0500

    Uncommented MySQL Queries

--- queries.sql
+++ queries.sql
@@ -1,24 +1,24 @@
 -- Part 1: Test it with SQL
 
--- 			column :				data type:
---			 id						int
---			 employer					varchar(255)
---			 name						varchar(255)
---			 skills					varchar(255)
+ 			column :				data type:
+			 id						int
+			 employer					varchar(255)
+			 name						varchar(255)
+			 skills					varchar(255)
 
 
 -- Part 2: Test it with SQL
--- SELECT name
--- FROM employer
--- WHERE location = "St. Louis City";
+ SELECT name
+ FROM employer
+ WHERE location = "St. Louis City";
 
 
 -- Part 3: Test it with SQL
--- DROP TABLE job;
+ DROP TABLE job;
 
 -- Part 4: Test it with SQL
 
---SELECT * FROM skill
---INNER JOIN job_skills ON skill.id = job_skills.skills_id
---WHERE job_skills.jobs_id IS NOT NULL
---ORDER BY name ASC;
+SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
